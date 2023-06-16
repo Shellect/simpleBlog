91c41d4a9004ae2a9d5f2fa3990c434b0f19298b
your name <you@example.com>
2023-06-16 08:31:12 +0300

    add config

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'dbname' => '',
+    'app_dir' => __DIR__ . '/app',
+    'template_dir' => __DIR__ . '/templates/',
+    'template_cache_path' => 'cache/',
+    'template_cache_enabled' => false,
+    'static_path' => 'static/',
+    'pretty_url' => false,
+];
